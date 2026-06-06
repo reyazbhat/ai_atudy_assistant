8f2aacac2690efa25eda38451167cbcf3c16c82a
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2026-06-06 11:30:56 +0530

    updated the model

--- core/models.py
+++ core/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Note(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
     title = models.CharField(max_length=100)
     file = models.FileField(upload_to='uploads/')
     content = models.TextField(blank=True)
     summary = models.TextField(blank=True)
+
     mcqs = models.TextField(blank=True)
+
     def __str__(self):
         return self.title
+
+
+
