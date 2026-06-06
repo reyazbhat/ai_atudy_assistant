a00999053ead1f12731eca9a4d1f28c7c4ff5853
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2026-06-06 11:34:53 +0530

    updated the paths

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.home, name='home'),
+
     path('upload/', views.upload, name='upload'),
+
     path('note/<int:id>/', views.note_detail, name='note_detail'),
+
+    path(
+        'login/',
+        views.CustomLoginView.as_view(),
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        views.logout_view,
+        name='logout'
+    ),
 ]
